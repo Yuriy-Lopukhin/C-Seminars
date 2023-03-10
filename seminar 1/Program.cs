a54149d3335f26d4ec8b8d9adccf56205d5a5459
Yuriy <yuriylopukhin@mail.ru>
2023-03-10 08:29:31 +0300

    решения задач, разобранные на семинарах!

---
+++ seminar 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+
+string UserInputStr = Console.ReadLine() ?? "";
+int UserNumber = int.Parse(UserInputStr);
+
+int result = UserNumber * UserNumber;
+
+Console.WriteLine($"{UserNumber} -> {result}");
+
